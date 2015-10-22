78e2285f276b4cd39c049f2765bf96f6f8c7a796
Tim Phillips <tim.p.phillips@gmail.com>
2015-10-22 16:12:01 -0700

    add url

--- .make.sh
+++ .make.sh
@@ -2,6 +2,7 @@
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
 ############################
 
 ########## Variables
