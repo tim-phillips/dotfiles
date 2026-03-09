e146d4031bb1a30742c66e7b3cb5174b8a3473a0
Tim Phillips <tim@classkick.com>
2026-03-09 11:33:58 -0700

    vim: remove copilot node path

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
-vim.g.copilot_node_command = "/usr/local/bin/node"
