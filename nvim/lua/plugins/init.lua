66fc47d1794463e087c07e577a19c5509ebeaca1
Tim Phillips <3578574+tim-phillips@users.noreply.github.com>
2026-03-09 11:54:11 -0700

    vim: prevent line length warnings in markdown

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -43,4 +43,14 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     enabled = false,
   },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          prepend_args = { "--config", vim.env.HOME .. "/.config/LazyVimDev/.markdownlint-cli2.yaml", "--" },
+        },
+      },
+    },
+  },
 }
